e7ead4452e86c454c5339dac23c1a24bd819a2df
Nahuel <nanocode2022@gmail.com>
2023-01-27 13:59:03 -0300

    acutalizacion cartel

--- app.js
+++ app.js
@@ -4,6 +4,55 @@ let nameCard = document.querySelector('.name');
 let month = document.querySelector('.month')
 let year = document.querySelector('.year')
 let cvc = document.querySelector('.cvc')
+let buttonEnviar = document.getElementById('btnEnviar');
+let container = document.getElementById('containerFormulario');
+let buttonContinue = document.getElementById('btnContinue');
+let containerAccept = document.getElementById('containerAceptar');
+
+buttonEnviar.addEventListener('click',(event)=>{
+    event.preventDefault()
+    formulario.remove()
+    // const divCreado = document.createElement('div');
+    // divCreado.className = 'containerAccept';
+    // divCreado.id = 'containerAceptar';
+    containerAccept.className = 'containerAccept'
+    const accept = `
+        <div class="tick">
+        ✔
+        </div>
+            <h2>THANK YOU!</h2>
+            <P>Lorem ipsum dolor sit amet.</P>
+        <button class="button" id="btnContinue" onclick="miFunc()">Continue</button>
+    `;
+    // divCreado.innerHTML = accept;
+    containerAccept.innerHTML = accept
+    container.append(containerAccept);
+})
+
+const miFunc = ()=>{
+    containerAccept.remove()
+
+    // const formInput = `
+    //     <label>Card Number</label>
+    //     <input type="text" maxlength="19" placeholder="e.g. 0000 0000 0000 0000" id="inputNroCard" class=" input">
+    //     <label>Card Name</label>
+    //     <input type="text" placeholder="nombre" id="cardName" class="input">
+    //     <span>
+    //         <label>Exp. Date (MM/YY)</label>
+    //         <label style="margin-left: 30px">Cvc</label>
+    //     </span>
+    //     <span>
+    //         <input type="text" maxlength="2" placeholder="MM" id="cardMonth" class="cardMonth inputDate">
+    //         <input type="text" maxlength="2" placeholder="YY" id="cardYear" class="cardYear inputDate">
+       
+    //         <input type="text" maxlength="3" placeholder="e.g 123" id="clave" class="cardCvc">
+    //     </span>
+    //     <button id="btnEnviar" class="button">Confirm</button>
+    // `;
+    // formulario.innerHTML = formInput;
+    container.append(formulario)
+}
+
 
 
 formulario.inputNroCard.addEventListener('keyup', (e)=>{
